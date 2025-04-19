8872d731b04ca1a8f2bca2e97dd9fcb5f608766c
BowtiedHarpyEagle <140409172+BowtiedHarpyEagle@users.noreply.github.com>
2025-04-19 17:49:13 -0500

    04 17:00

--- src/RebaseToken.sol
+++ src/RebaseToken.sol
@@ -18,18 +18,19 @@ contract RebaseToken is ERC20{
 
     error RebaseToken__InterestRateCanOnlyDecrease(uint256 oldInterestRate, uint256 newInterestRate);
 
-    uint256 private s_interestRate = 5e10; // interest rate per second in 1e18 
+    uint256 private s_interestRate = 5e10; // interest rate per second in 1e18
+    mapping (address => uint256) private s_userInterestRate;
 
     constructor() ERC20("RebaseToken", "RBT") {
 
     }
 
-/*
-* @notice This function sets the new interest rate for the contract.
-* @param _newInterestRate The new interest rate for the contract.
-* @dev The interest rate can only be decreased. If the new interest rate is greater than
-* @dev the current interest rate, the transaction will revert.
-*/
+    /*
+    * @notice This function sets the new interest rate for the contract.
+    * @param _newInterestRate The new interest rate for the contract.
+    * @dev The interest rate can only be decreased. If the new interest rate is greater than
+    * @dev the current interest rate, the transaction will revert.
+    */
 
     function setInterestRate(uint256 _newInterestRate)  external {
         if (_newInterestRate >= s_interestRate) {
@@ -39,4 +40,29 @@ contract RebaseToken is ERC20{
         s_interestRate = _newInterestRate;
         emit InterestRateSet(_newInterestRate);
     }
+
+    function mint(address _to, uint256 _amount) external {
+        _mintAccruedInterest(_to);
+        s_userInterestRate[_to] = s_interestRate;
+        _mint(_to, _amount);
+    }
+
+    function _mintAccruedInterest(address _user) internal {
+        // (1) find the current balance of already minted tokens to the user -> principal
+        // (2) calculate the current balance including interest
+        // (3) calculate difference between the current balance and the new balance
+        // call _mint to mint the necessary amount
+    }
+
+    function getUserInterestRate(address _user) external view returns (uint256) {
+        return s_userInterestRate[_user];
+    }
+
+    function getInterestRate() external view returns (uint256) {
+        return s_interestRate;
+    }
+
+
+
+
 }
