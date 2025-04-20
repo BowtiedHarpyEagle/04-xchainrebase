f8a2d6e51c19959b2533ea859441e3963df9969f
BowtiedHarpyEagle <140409172+BowtiedHarpyEagle@users.noreply.github.com>
2025-04-20 18:26:07 -0500

    8 1:35 starting Vault

---
+++ src/Vault.sol
@@ -0,0 +1,7 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+contract Vault{
+    // We need to pass the token address to the constructor
+    // create a deposit function that mints the tokens to the user
+}
