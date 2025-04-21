e42d5989eb952983808c5aaf029125cd3a119cb1
BowtiedHarpyEagle <140409172+BowtiedHarpyEagle@users.noreply.github.com>
2025-04-21 16:06:38 -0500

    9 5:00 setUp() complete

--- test/RebaseToken.t.sol
+++ test/RebaseToken.t.sol
@@ -8,5 +8,23 @@ import {Vault} from "../src/Vault.sol";
 
 import {IRebaseToken} from "../src/interfaces/IRebaseToken.sol";
 
-contract RebaseTokenTest is Test {}
+contract RebaseTokenTest is Test {
+    RebaseToken private rebaseToken;
+    Vault private vault;
+
+    address public  owner = makeAddr("owner");
+    address public  user = makeAddr("user");
+    
+    function setUp() public {
+        // Deploy the RebaseToken contract
+        vm.startPrank(owner);
+        rebaseToken = new RebaseToken();
+        // we cannot typecast the rebaseToke to IRebaseToken directly so
+        // we need to use typecast address to IRebaseToken 
+        vault = new Vault(IRebaseToken(address(rebaseToken)));
+        rebaseToken.grantMintAndBurnRole(address(vault));
+        vm.stopPrank();
+    }
+
+}
 
