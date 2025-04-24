063a506aa57950bd73d1fc63036f34cd39e01579
BowtiedHarpyEagle <140409172+BowtiedHarpyEagle@users.noreply.github.com>
2025-04-24 15:17:02 -0500

    16 9:05

--- src/RebaseTokenPool.sol
+++ src/RebaseTokenPool.sol
@@ -4,5 +4,5 @@ pragma solidity ^0.8.24;
 import {TokenPool} from "@ccip/contracts/src/v0.8/ccip/pools/TokenPool.sol";
 
 contract RebaseTokenPool is TokenPool {
-    constructor() TokenPool() {}
+    constructor(IERC20 _token, address[] memory _allowlist, address _rmnProxy, address _router) TokenPool(_token, _allowlist, _rmnProxy, _router) {}
 }
