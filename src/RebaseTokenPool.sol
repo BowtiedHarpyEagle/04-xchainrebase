37cb3e6827d196606e43996edcf5ceac222e4729
BowtiedHarpyEagle <140409172+BowtiedHarpyEagle@users.noreply.github.com>
2025-05-09 18:20:38 -0500

    16 13:08 imports correct, functions lockOrBurn and releaseOrMint start

--- src/RebaseTokenPool.sol
+++ src/RebaseTokenPool.sol
@@ -1,8 +1,22 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.24;
 
-import {TokenPool} from "@ccip/contracts/src/v0.8/ccip/pools/TokenPool.sol";
+import {TokenPool} from "@chainlink/ccip/pools/TokenPool.sol";
+import {Pool} from "@chainlink/ccip/libraries/Pool.sol";
+import {IERC20} from "@chainlink/vendor/openzeppelin-solidity/v4.8.3/contracts/token/ERC20/IERC20.sol";
+
 
 contract RebaseTokenPool is TokenPool {
-    constructor(IERC20 _token, address[] memory _allowlist, address _rmnProxy, address _router) TokenPool(_token, _allowlist, _rmnProxy, _router) {}
+    constructor(IERC20 _token, address[] memory _allowlist, address _rmnProxy, address _router) TokenPool(_token, 18, _allowlist, _rmnProxy, _router) {}
+
+    function lockOrBurn(
+    Pool.LockOrBurnInV1 calldata lockOrBurnIn
+  ) external returns (Pool.LockOrBurnOutV1 memory lockOrBurnOut) {
+      
+  }
+
+  function releaseOrMint(
+    Pool.ReleaseOrMintInV1 calldata releaseOrMintIn
+  ) external returns (Pool.ReleaseOrMintOutV1 memory) {}
+
 }
