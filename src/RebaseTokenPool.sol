9c84b651500980bfd0559152565fa85c66a8f816
BowtiedHarpyEagle <140409172+BowtiedHarpyEagle@users.noreply.github.com>
2025-05-10 15:44:07 -0500

    16 21:22

--- src/RebaseTokenPool.sol
+++ src/RebaseTokenPool.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.24;
 import {TokenPool} from "@chainlink/ccip/pools/TokenPool.sol";
 import {Pool} from "@chainlink/ccip/libraries/Pool.sol";
 import {IERC20} from "@chainlink/vendor/openzeppelin-solidity/v4.8.3/contracts/token/ERC20/IERC20.sol";
-
+import {IRebaseToken} from "./interfaces/IRebaseToken.sol";
 
 contract RebaseTokenPool is TokenPool {
     constructor(IERC20 _token, address[] memory _allowlist, address _rmnProxy, address _router) TokenPool(_token, 18, _allowlist, _rmnProxy, _router) {}
@@ -14,6 +14,12 @@ contract RebaseTokenPool is TokenPool {
   ) external returns (Pool.LockOrBurnOutV1 memory lockOrBurnOut) 
   {
     _validateLockOrBurn(lockOrBurnIn);
+    address receiver = abi.decode(lockOrBurnIn.receiver, (address));
+    uint256 userInterestRate = IRebaseToken(address(i_token)).getUserInterestRate(receiver);
+    IRebaseToken(address(i_token)).burn(address(this), lockOrBurnIn.amount);
+    lockOrBurnOut = Pool.LockOrBurnOutV1({
+      destTokenAddress: 
+    });
   }
 
   function releaseOrMint(
