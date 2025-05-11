245d7e657429b89ba9aa250cb1043f07943ddf40
BowtiedHarpyEagle <140409172+BowtiedHarpyEagle@users.noreply.github.com>
2025-05-11 17:53:16 -0500

    18 9:25 import CCIPLocalSimulatiorFork

--- test/CrossChain.t.sol
+++ test/CrossChain.t.sol
@@ -3,6 +3,8 @@ pragma solidity ^0.8.24;
 
 import {Test, console} from "forge-std/Test.sol";
 
+import {CCIPLocalSimulatorFork} from "@chainlink-local/src/ccip/CCIPLocalSimulatorFork.sol";
+
 import {RebaseToken} from "../src/RebaseToken.sol";
 import {Vault} from "../src/Vault.sol";
 import {RebaseTokenPool} from "../src/RebaseTokenPool.sol";
@@ -10,6 +12,12 @@ import {RebaseTokenPool} from "../src/RebaseTokenPool.sol";
 import {IRebaseToken} from "../src/interfaces/IRebaseToken.sol";
 
 contract CrossChainTest is Test {
-    function setUp() public {}
-    
+    uint256 ethSepoliaFork;
+    uint256 arbSepoliaFork;
+
+    function setUp() public {
+        ethSepoliaFork = vm.createSelectFork("sepolia-eth");
+        arbSepoliaFork = vm.createFork("sepolia-arb");
+    }
+
 }
