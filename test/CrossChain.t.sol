fc208f2b40d980222cfb1c3f4dda7a7b5354fa2b
BowtiedHarpyEagle <140409172+BowtiedHarpyEagle@users.noreply.github.com>
2025-05-12 11:54:12 -0500

    20 3:30

--- test/CrossChain.t.sol
+++ test/CrossChain.t.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.24;
 
 import {Test, console} from "forge-std/Test.sol";
 
-import {CCIPLocalSimulatorFork} from "@chainlink-local/src/ccip/CCIPLocalSimulatorFork.sol";
+import {CCIPLocalSimulatorFork, Register} from "@chainlink-local/src/ccip/CCIPLocalSimulatorFork.sol";
 
 import {RebaseToken} from "../src/RebaseToken.sol";
 import {Vault} from "../src/Vault.sol";
@@ -12,10 +12,22 @@ import {RebaseTokenPool} from "../src/RebaseTokenPool.sol";
 import {IRebaseToken} from "../src/interfaces/IRebaseToken.sol";
 
 contract CrossChainTest is Test {
+    address constant owner = makeAddr("owner");
     uint256 ethSepoliaFork;
     uint256 arbSepoliaFork;
 
     CCIPLocalSimulatorFork ccipLocalSimulatorFork;
+
+    RebaseToken ethSepoliaToken;
+    RebaseToken arbSepoliaToken;
+
+    Vault vault;
+
+    RebaseTokenPool ethSepoliaPool;
+    RebaseTokenPool arbSepoliaPool;
+
+    Register.NetworkDetails ethSepoliaNetworkDetails;
+    Register.NetworkDetails arbSepoliaNetworkDetails;
 
     function setUp() public {
         ethSepoliaFork = vm.createSelectFork("sepolia-eth");
@@ -23,6 +35,21 @@ contract CrossChainTest is Test {
 
         ccipLocalSimulatorFork = new CCIPLocalSimulatorFork();
         vm.makePersistent(address(ccipLocalSimulatorFork));
+
+        //deploy and configure on ethSepolia
+        ethSepoliaNetworkDetails = ccipLocalSimulatorFork.getNetworkDetails(block.chainId);
+        vm.startPrank(owner);
+        ethSepoliaToken = new RebaseToken();
+        vault = new Vault(IRebaseToken(address(ethSepoliaToken)));
+        ethSepoliaPool = new RebaseTokenPool();
+        vm.stopPrank();
+
+        //deploy and configure on arbSepolia
+        vm.selectFork(arbSepoliaFork);
+        arbSepoliaNetworkDetails = ccipLocalSimulatorFork.getNetworkDetails(block.chainId);
+        vm.startPrank(owner);
+        arbSepoliaToken = new RebaseToken();
+        vm.stopPrank();
     }
 
 }
